65adca1c7cdab27ccc4f403b4efbd0889a40fdf6
huzaifa-emumba <huzaifa.arshad@emumba.com>
2025-09-18 12:24:23 +0500

    feat: implement automatic login and restore AuthRoutes in routing

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
@@ -22,17 +21,25 @@ import { SET_DARKMODE } from './store/actions'
 
 const App = () => {
     const customization = useSelector((state) => state?.customization)
+    const user = useSelector((state) => state?.auth?.user)
     const loginApi = useApi(authApi.login)
-    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user) return
+        const doLogin = async () => {
+            await loginApi.request({ email: 'huzaifa.arshad@emumba.com', password: 'Emumba@123' })
+        }
+
+        doLogin()
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user])
 
     useEffect(() => {
         if (loginApi.data) {
             store.dispatch(loginSuccess(loginApi.data))
             store.dispatch({ type: SET_DARKMODE, isDarkMode: true })
-            navigate(location.state?.path || '/chatflows')
         }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [loginApi.data])
 
     return (
